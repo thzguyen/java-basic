b20ea8fd0b1981d7e41042e83dcff5124bd0f746
thazhhh <128007886+thazhhh@users.noreply.github.com>
2024-06-19 16:12:20 +0900

    first commit

---
+++ src/HelloJava.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class HelloJava {
+    public static void main(String[] args){
+//        출력
+        System.out.println("helle java");
+
+//        입력 받고 출력
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextLine());
+
+//        해당 내용 각 주석으로 정리하기
+
+    }
+}
